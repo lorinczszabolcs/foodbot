fc106463bb89a5f8d94df1be2422acbe6d1033fe
Szabolcs Lorincz <lorincz_szabolcs@yahoo.com>
2020-11-07 00:11:11 +0200

    Some stuff

---
+++ aitoconnection.py
@@ -0,0 +1,14 @@
+from aito.client import AitoClient
+import aito.api as aito_api
+
+class AitoConnection():
+    def __init__(self, instance_url, api_key):
+        self.instance_url = instance_url
+        self.api_key = api_key
+        self.client = AitoClient(instance_url=self.instance_url, api_key=self.api_key)
+
+    def query(self, query):
+        res = aito_api.generic_query(client=self.client, query=query)
+
+    def recommend(self, rec_query, top=False):
+        res = aito_api.recommend(client, rec_query.update({'limit': top})) if top else aito_api.recommend(client, rec_query)
