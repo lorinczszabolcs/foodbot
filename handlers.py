8429378f6d529d9d664d1d09e914becbe3cfed9e
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-11-07 20:41:07 +0200

    Add duplicate control on dataset when join is pressed

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-import time
+from datetime import datetime
 import sys
 import logging
 
@@ -15,51 +15,155 @@ from telegram import (
 )
 from telegram.ext import (
     Updater,
-    CommandHandler,
     MessageHandler,
     Filters,
     CallbackContext,
-    CallbackQueryHandler
+    CommandHandler,
+    CallbackQueryHandler,
 )
 
+import pandas as pd
+from enum import Enum
+
+
+class PandasDB:
+    def read(self, path="database.csv") -> pd.DataFrame:
+        return pd.read_csv("database.csv")
+
+    def write(self, dataframe: pd.DataFrame) -> None:
+        dataframe.to_csv("database.csv", index=False, header=True)
+
+
 # Handlers
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+def start(update: Update, context: CallbackContext):
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    update.message.reply_text(
+        text="Hey welcome to FoodVote! Type /vote to get started.",
+    )
 
-def button(update, context):
+
+def vote(update: Update, context: CallbackContext):
+
+    timestamp = datetime.now().isoformat()
+    voting_id = update.effective_chat.id
+    user_id = update.effective_user.id
+    confirmed = False
+
+    db = PandasDB().read()
+    PandasDB().write(
+        db.append(
+            pd.DataFrame(
+                {
+                    "timestamp": timestamp,
+                    "voting_id": voting_id,
+                    "user_id": user_id,
+                    "confirmed": confirmed,
+                },
+                index=[0],
+            ),
+            ignore_index=True,
+        )
+    )
+
     keyboard = [
         [
-            InlineKeyboardButton("Option 1", callback_data='1'),
-            InlineKeyboardButton("Option 2", callback_data='2'),
+            InlineKeyboardButton("Join!", callback_data="join"),
+            InlineKeyboardButton("Go to bot", url="https://t.me/foodvote_bot"),
+        ],
+        [
+            InlineKeyboardButton("Close joining phase", callback_data="close"),
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(
+        "Voting session started, click the button below to join",
+        reply_markup=reply_markup,
+    )
+
+
+def count_join(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    if query.data == "join":
+
+        timestamp = datetime.now().isoformat()
+        voting_id = update.effective_chat.id
+        user_id = update.effective_user.id
+        confirmed = False
+
+        db = PandasDB().read()
+
+        if (voting_id, user_id) not in zip(db.voting_id.tolist(), db.user_id.tolist()):
+            PandasDB().write(
+                db.append(
+                    pd.DataFrame(
+                        {
+                            "timestamp": timestamp,
+                            "voting_id": voting_id,
+                            "user_id": user_id,
+                            "confirmed": confirmed,
+                        },
+                        index=[0],
+                    ),
+                    ignore_index=True,
+                )
+            )
+    elif query.data == "close":
+        query.edit_message_text(
+            "Thank you for joining, please look at your private chats with @foodvote_bot and send the start command."
+        )
+
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+def button(update: Update, context: CallbackContext):
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "Option 1", url="tg://t.me/MattiaDoStuff_bot", callback_data="1"
+            ),
         ]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    context.bot.send_message(
+        query.message.chat_id, "Please choose:", reply_markup=reply_markup
+    )
+
+    # update.message.reply_text("Please choose:", reply_markup=reply_markup)
 
 
 def button_answer(update: Update, context: CallbackContext) -> None:
-    
-    query = update.callback_query
 
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    query.answer()
+    pass
+    # query = update.callback_query
 
-    keyboard = [
-        [
-            InlineKeyboardButton("Option 1", callback_data='1'),
-            InlineKeyboardButton("Option 2", callback_data='2'),
-        ]
-    ]
+    # # CallbackQueries need to be answered, even if no notification to the user is needed
+    # # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    # query.answer()
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    # keyboard = [
+    #     [
+    #         InlineKeyboardButton("Option 1", callback_data="1"),
+    #         InlineKeyboardButton("Option 2", callback_data="2"),
+    #     ]
+    # ]
 
-    context.bot.send_message(query.message.chat_id, 'Please choose:', reply_markup=reply_markup)
+    # reply_markup = InlineKeyboardMarkup(keyboard)
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    # context.bot.send_message(
+    #     query.message.chat_id, "Please choose:", reply_markup=reply_markup
+    # )
+
+
+def unknown(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that command.",
+    )
--- telegrambot.py
+++ telegrambot.py
@@ -19,36 +19,35 @@ from telegram.ext import (
     MessageHandler,
     Filters,
     CallbackContext,
-    CallbackQueryHandler
+    CallbackQueryHandler,
+    InlineQueryHandler,
 )
 
-from handlers import (
-    start,
-    unknown,
-    button_answer,
-    button
-)
+from handlers import start, unknown, button_answer, button, vote, echo, count_join
+import pandas as pd
 
 # tokens to interact with the bots
 # 1405956615:AAGPs1Ta65Y1W8GBWlOKkzFeQejGsCviBXo new_test_bot
 # 1440330193:AAHmGIYnqllLUBVl97DVflG48D_EPW0zZPI freefood_bot
 # 1479496566:AAHOsvWBa6OQOrV0nuORHuJQkYTEIz8peik freeefoood_bot
-
-
+# 1476500295:AAG83jTZTGzZz13M2zHfseIhXUg74cj_ApU foodvote_bot
 
 
 if __name__ == "__main__":
-    
-    # access the bot
-    updater = Updater(token='1405956615:AAGPs1Ta65Y1W8GBWlOKkzFeQejGsCviBXo', use_context=True)
-    dispatcher = updater.dispatcher
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('button', button))
-    dispatcher.add_handler(CallbackQueryHandler(button_answer))
+    # access the bot
+    updater = Updater(
+        token="1476500295:AAG83jTZTGzZz13M2zHfseIhXUg74cj_ApU", use_context=True
+    )
+    dispatcher = updater.dispatcher
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("vote", vote))
+    dispatcher.add_handler(CallbackQueryHandler(count_join))
     unknown_handler = MessageHandler(Filters.command, unknown)
     dispatcher.add_handler(unknown_handler)
 
